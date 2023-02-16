bf40c441de3dfb7c6178a7400803fdfeac2fe27f
alexeira <alex.pincheira06@gmail.com>
2023-02-15 23:13:34 -0300

    format

--- test/index.ts
+++ test/index.ts
@@ -1,45 +1,45 @@
-var test = require('tape')
+const test = require('tape')
 
-var dotpather = require('../index.js')
+const dotpather = require('../index.js')
 
 test('returns value at property', function (t) {
   t.plan(2)
 
-  var lookup = dotpather('check')
+  const lookup = dotpather('check')
 
-  t.equal(lookup({check: 'woo'}), 'woo')
-  t.deepEqual(lookup({check: {derp: true}}), {derp: true})
+  t.equal(lookup({ check: 'woo' }), 'woo')
+  t.deepEqual(lookup({ check: { derp: true } }), { derp: true })
 })
 
 test('finds nested values', function (t) {
   t.plan(1)
 
-  var lookup = dotpather('check.that.attribute')
+  const lookup = dotpather('check.that.attribute')
 
-  t.equal(lookup({check: {that: {attribute: 666}}}), 666)
+  t.equal(lookup({ check: { that: { attribute: 666 } } }), 666)
 })
 
 test('returns undefined if path not found', function (t) {
   t.plan(1)
 
-  var lookup = dotpather('invalid.path')
+  const lookup = dotpather('invalid.path')
 
-  t.equal(lookup({valid: {path: true}}), void 0)
+  t.equal(lookup({ valid: { path: true } }), void 0)
 })
 
 test('properly returns falsey values', function (t) {
   t.plan(2)
 
-  var lookup = dotpather('falsey.value')
+  const lookup = dotpather('falsey.value')
 
-  t.equal(lookup({falsey: {value: false}}), false)
-  t.equal(lookup({falsey: {value: null}}), null)
+  t.equal(lookup({ falsey: { value: false } }), false)
+  t.equal(lookup({ falsey: { value: null } }), null)
 })
 
 test('works on arrays also', function (t) {
   t.plan(1)
 
-  var lookup = dotpather('arr.1')
+  const lookup = dotpather('arr.1')
 
-  t.equal(lookup({arr: [44, 22]}), 22)
+  t.equal(lookup({ arr: [44, 22] }), 22)
 })
